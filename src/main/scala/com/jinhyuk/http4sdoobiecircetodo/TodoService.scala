aea7d8c8544696c0cc4472a883ba1c6b40c3e0e1
rylan.k <rylan.k@kakaopaycorp.com>
2019-07-05 09:39:21 +0900

    add import org.http4s.circe.CirceEntityCodec._ to use default entitydecoder and entityencoder

--- src/main/scala/com/jinhyuk/http4sdoobiecircetodo/TodoService.scala
+++ src/main/scala/com/jinhyuk/http4sdoobiecircetodo/TodoService.scala
@@ -7,7 +7,7 @@ import org.http4s.syntax._
 import org.http4s.dsl.io._
 import org.http4s.implicits._
 import org.http4s.server.blaze._
-import org.http4s.EntityDecoder
+import org.http4s.circe.CirceEntityCodec._
 import org.http4s.MediaType
 import io.circe.generic.auto._
 import io.circe.syntax._
@@ -15,10 +15,6 @@ import io.circe._
 import org.http4s.circe._
 
 object TodoService {
-  implicit private val todoEncoder = jsonEncoderOf[IO, Todo]
-  implicit private val todoListEncoder = jsonEncoderOf[IO, Seq[Todo]]
-  implicit private val todoRequestDecoder = jsonOf[IO, TodoRequest]
-
   val service = HttpRoutes.of[IO] {
     case GET -> Root / "api" / "todos" => TodoRepository.findAll.flatMap(Ok(_))
     case GET -> Root / "api" / "todos" / IntVar(id) =>
